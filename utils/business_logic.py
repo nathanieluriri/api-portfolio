be52614f36d902f4e926fa2c51df002c6bde1564
nathanieluriri <uririnathaniel@gmail.com>
2025-02-25 00:12:41 +0100

    added business logic

---
+++ utils/business_logic.py
@@ -0,0 +1,16 @@
+
+def get_all_projects_func(DB_TYPE,):
+    pass
+
+def create_project_func(**kwargs):
+    pass
+
+
+def update_project_func(**kwargs):
+    pass
+
+def get_particular_project_func(projectId:int):
+    pass
+
+def delete_project_func(projectId:int):
+    pass
